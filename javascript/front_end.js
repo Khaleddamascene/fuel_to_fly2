7e516b1b431ec6508ea1878f7c6a84ae5cde7f4f
Andrei1033 <andrei.tsizikov00@gmail.com>
2025-11-28 19:55:39 +0200

    peli sivu front end tehty. toimiva kartan tekeminen on alkuvaihessa

---
+++ javascript/front_end.js
@@ -0,0 +1,6 @@
+// Peli sivun kartta 
+const map = L.map('karta_tieto_div').setView([60.1699, 24.9384], 12); // Tähän pitä laita lat ja lon muuttujat että karta näytä nykyinen paika
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+}).addTo(map);
