725bc959d0f5694f7b0220d042aad2f765fcd7b7
AaroHaavisto <118162246+AaroHaavisto@users.noreply.github.com>
2025-12-05 14:19:28 +0200

    Loppusivu Backend
    Loppusivun backend pitäisi nyt toimia

--- paaohjelma_python/db.py
+++ paaohjelma_python/db.py
@@ -5,7 +5,8 @@ def get_connection():
         host='127.0.0.1',
         port=3306,
         database='fuel_to_fly',
-        user='Dornaraj',
-        password='123',
+        user='root',
+        password='1234',
+
         autocommit=True
     )
